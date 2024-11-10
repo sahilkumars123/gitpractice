176ab376823c7d578c735d0de15cd70d1e832c92
Sahil Kumar <sahil.kumar9274@gmail.com>
2024-11-10 19:22:57 +0530

    added new files

---
+++ src/main/java/HomePage.java
@@ -0,0 +1,9 @@
+
+public class HomePage {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("this is my homepage");
+	}
+
+}
---
+++ src/test/java/LoginPageTest.java
@@ -0,0 +1,9 @@
+
+public class LoginPageTest {
+	
+public static void main(String[] args) {
+		
+		System.out.println("this is my loginpage test");
+	}
+
+}
